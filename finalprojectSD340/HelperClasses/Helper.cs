ef347928af61d85e8d5f170d439229de54a022d1
remone2 <jamodinm@yahoo.com>
2022-04-03 21:00:52 -0500

    Add Abstract Helper and move around other classes

---
+++ finalprojectSD340/HelperClasses/Helper.cs
@@ -0,0 +1,11 @@
+﻿namespace finalprojectSD340.HelperClasses
+{
+    public abstract class Helper
+    {
+        public abstract void Add();
+
+        public abstract void Delete();
+
+        public abstract void Update();
+    }
+}
---
+++ finalprojectSD340/HelperClasses/ProjectHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace finalprojectSD340.HelperClasses
+{
+    public class ProjectHelper : Helper
+    {
+        public override void Add()
+        {
+
+        }
+
+        public override void Delete()
+        {
+            
+        }
+
+        public override void Update()
+        {
+            
+        }
+    }
+}
